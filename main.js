aebdcf259ca3715d196233e14a0edf80916c7c9e
Martin Sosa <mr.martinsosa@gmail.com>
2018-03-21 20:24:05 -0400

    Laid out skeleton for navbar jump

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+	let menuJump = document.querySelector("change-color")
+	menuJump.addEventListener("onclick", (event) => {
+		if(menuJump.value === "About Us"){
+			window.scroll({
+				top: 0, //location in px
+				left: 0,
+				behavior: "smooth"
+			})
+		}else if(menuJump.value === "Reviews"){
+			window.scroll({
+				top: 0, 
+				left: 0,
+				behavior: "smooth"
+			})
+		}else if(menuJump.value === "The Team"){
+			window.scroll({
+				top: 0, 
+				left: 0,
+				behavior: "smooth"
+			})
+		}else if(menuJump.value === "Contact Us"){
+			window.scroll({
+				top: 0, 
+				left: 0,
+				behavior: "smooth"
+			})
+		}
+	})
+})
