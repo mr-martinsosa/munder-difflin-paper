6f9470d4f5677e6176d459ae822120f613ac9a14
Martin Sosa <mr.martinsosa@gmail.com>
2018-03-22 11:51:28 -0400

    made navbar sticky

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+// 	let menuJump = document.querySelectorAll("change-color")
+// 	menuJump.addEventListener("onclick", (event) => {
+// 		if(menuJump.value === "About Us"){
+// 			window.scroll({
+// 				top: 0, //location in px
+// 				left: 0,
+// 				behavior: "smooth"
+// 			})
+// 		}else if(menuJump.value === "Reviews"){
+// 			window.scroll({
+// 				top: 0, 
+// 				left: 0,
+// 				behavior: "smooth"
+// 			})
+// 		}else if(menuJump.value === "The Team"){
+// 			window.scroll({
+// 				top: 0, 
+// 				left: 0,
+// 				behavior: "smooth"
+// 			})
+// 		}else if(menuJump.value === "Contact Us"){
+// 			window.scroll({
+// 				top: 0, 
+// 				left: 0,
+// 				behavior: "smooth"
+// 			})
+// 		}
+// 	})
+// })
+
 document.addEventListener("DOMContentLoaded", (event) => {
-	let menuJump = document.querySelectorAll("change-color")
-	menuJump.addEventListener("onclick", (event) => {
-		if(menuJump.value === "About Us"){
-			window.scroll({
-				top: 0, //location in px
-				left: 0,
-				behavior: "smooth"
-			})
-		}else if(menuJump.value === "Reviews"){
-			window.scroll({
-				top: 0, 
-				left: 0,
-				behavior: "smooth"
-			})
-		}else if(menuJump.value === "The Team"){
-			window.scroll({
-				top: 0, 
-				left: 0,
-				behavior: "smooth"
-			})
-		}else if(menuJump.value === "Contact Us"){
-			window.scroll({
-				top: 0, 
-				left: 0,
-				behavior: "smooth"
-			})
-		}
-	})
+	
 })
