e66a95bb1404e7f7a8d8a5a861b80d7317766d12
Martin Sosa <mr.martinsosa@gmail.com>
2018-03-22 14:09:57 -0400

    fixed navbar jumping and highlighting and fixed broken paper repeat

--- main.js
+++ main.js
@@ -31,4 +31,59 @@
 
 document.addEventListener("DOMContentLoaded", (event) => {
 	
+	console.log(window.scrollY)
+	window.addEventListener("scroll", (event) => {
+		console.log(window.scrollY)
+		let grabNav = document.querySelectorAll(".change-color")
+		
+		let aboutUs = grabNav[1]
+		let ourPaper = grabNav[2]
+		let theTeam = grabNav[3]
+		let reviews = grabNav[4]
+		let contactUs = grabNav[5]
+
+		if(window.scrollY < 200){
+			aboutUs.classList.add("set-color")
+
+			ourPaper.classList.remove("set-color")
+			theTeam.classList.remove("set-color")
+			reviews.classList.remove("set-color")
+			contactUs.classList.remove("set-color")
+		}else if(window.scrollY > 131 && window.scrollY < 1799){
+			ourPaper.classList.add("set-color")
+
+			aboutUs.classList.remove("set-color")
+			theTeam.classList.remove("set-color")
+			reviews.classList.remove("set-color")
+			contactUs.classList.remove("set-color")
+		}else if(window.scrollY > 1800 && window.scrollY < 2700){
+			theTeam.classList.add("set-color")
+
+			aboutUs.classList.remove("set-color")
+			ourPaper.classList.remove("set-color")
+			reviews.classList.remove("set-color")
+			contactUs.classList.remove("set-color")
+		}else if(window.scrollY > 2701 && window.scrollY < 3078){
+			reviews.classList.add("set-color")
+
+			aboutUs.classList.remove("set-color")
+			ourPaper.classList.remove("set-color")
+			theTeam.classList.remove("set-color")
+			contactUs.classList.remove("set-color")
+		}else if(window.scrollY > 3079){
+			contactUs.classList.add("set-color")
+
+			aboutUs.classList.remove("set-color")
+			ourPaper.classList.remove("set-color")
+			theTeam.classList.remove("set-color")
+			reviews.classList.remove("set-color")
+		}//else {
+		// 	aboutUs.classList.remove("set-color")
+		// 	ourPaper.classList.remove("set-color")
+		// 	theTeam.classList.remove("set-color")
+		// 	reviews.classList.remove("set-color")
+		// 	contactUs.classList.remove("set-color")
+		// }
+	})
+
 })
