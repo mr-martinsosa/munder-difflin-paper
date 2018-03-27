b215466c86efb9a9f18a001ced8fa633819a553c
Martin Sosa <mr.martinsosa@gmail.com>
2018-03-26 22:27:01 -0400

    added fade in animations to load and scroll page

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+document.body.classList.add("fade-out")
+
 document.addEventListener("DOMContentLoaded", (event) => {
-	
-	console.log(window.scrollY)
+	window.addEventListener("mousemove", (event) => {
+		document.body.classList.remove("fade-out")
+	})
 	window.addEventListener("scroll", (event) => {
 		let grabNav = document.querySelectorAll(".change-color")
 		
@@ -10,13 +13,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		let reviews = grabNav[4]
 		let contactUs = grabNav[5]
 
-		if(window.scrollY < 200){
+		let grabSection = document.querySelectorAll("section")
+
+		let aboutSection = grabSection[0]
+		let paperSection = grabSection[1]
+		let teamSection = grabSection[2]
+		let reviewsSection = grabSection[3]
+		console.log(reviewsSection)
+		if(window.scrollY < 130){
 			aboutUs.classList.add("set-color")
 
 			ourPaper.classList.remove("set-color")
 			theTeam.classList.remove("set-color")
 			reviews.classList.remove("set-color")
 			contactUs.classList.remove("set-color")
+
+			aboutSection.classList.add("fade-in")
+
+			paperSection.classList.remove("fade-in")
+			teamSection.classList.remove("fade-in")
+			reviewsSection.classList.remove("fade-in")
 		}else if(window.scrollY > 131 && window.scrollY < 1799){
 			ourPaper.classList.add("set-color")
 
@@ -24,6 +40,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			theTeam.classList.remove("set-color")
 			reviews.classList.remove("set-color")
 			contactUs.classList.remove("set-color")
+
+			paperSection.classList.add("fade-in")
+
+			aboutSection.classList.remove("fade-in")
+			teamSection.classList.remove("fade-in")
+			reviewsSection.classList.remove("fade-in")
 		}else if(window.scrollY > 1800 && window.scrollY < 2700){
 			theTeam.classList.add("set-color")
 
@@ -31,6 +53,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			ourPaper.classList.remove("set-color")
 			reviews.classList.remove("set-color")
 			contactUs.classList.remove("set-color")
+
+			teamSection.classList.add("fade-in")
+
+			aboutSection.classList.remove("fade-in")
+			paperSection.classList.remove("fade-in")
+			reviewsSection.classList.remove("fade-in")
 		}else if(window.scrollY > 2701 && window.scrollY < 3078){ //set both contact and reviews to highlight since theyre close
 			reviews.classList.add("set-color")
 			contactUs.classList.add("set-color")
@@ -38,6 +66,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			aboutUs.classList.remove("set-color")
 			ourPaper.classList.remove("set-color")
 			theTeam.classList.remove("set-color")
+
+			reviewsSection.classList.add("fade-in")
+
+			aboutSection.classList.remove("fade-in")
+			paperSection.classList.remove("fade-in")
+			teamSection.classList.remove("fade-in")
 		}
 	})
 
